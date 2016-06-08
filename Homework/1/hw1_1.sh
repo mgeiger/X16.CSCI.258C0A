21b52d0692f604bb248c4b2898263582978f9f4d
Matthew J. Geiger <matthew.j.geiger@gmail.com>
2016-06-07 22:44:14 -0400

    HW #1 Completed.

---
+++ Homework/1/hw1_1.sh
@@ -0,0 +1,22 @@
+set -x
+printf "Homework 1, Part 1\n\n"
+printf "Question 1\n\n"
+printf "\n\nHW1.Q1.1)\n\n"
+uname
+
+printf "\n\nHW1.Q1.2)\n\n"
+uname -r
+
+printf "\n\nHW1.Q1.3)\n\n"
+uname -n
+hostname
+
+printf "\n\nHW1.Q1.4)\n\n"
+echo "Key setting to discard prior character is ctrl-h"
+echo "Key setting to discard entire typed line is ctrl-u"
+
+printf "\n\nHW1.Q1.5)\n\n"
+date
+
+printf "\n\nHW1.Q1.6)\n\n"
+who
---
+++ Homework/1/hw1_2.sh
@@ -0,0 +1,18 @@
+printf "\n\nHW1.Q1.7)\n\n"
+echo "please look at this one more"
+
+printf "\n\nHW1.Q1.8)\n\n"
+cd /
+
+printf "\n\nHW1.Q1.9)\n\n"
+pwd
+
+printf "\n\nHW1.Q1.10)\n\n"
+cd
+
+printf "\n\nHW1.Q1.11)\n\n"
+touch newfile.txt
+cp newfile.txt newerfile.txt
+
+printf "\n\nHW1.Q1.12)\n\n"
+ls -l newerfile.txt
